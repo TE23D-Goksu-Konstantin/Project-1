b20b38b6aa625a350cab4c05bbfec424a7eda979
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2026-01-13 12:35:04 +0100

    streamlined property variable verification

--- src/Properties/Property.java
+++ src/Properties/Property.java
@@ -3,7 +3,7 @@ package Properties;
 public abstract class Property 
 {
  
-    private int rooms;
+    protected int rooms;
     protected int cubic_Meter; //Cubic meters of the property
     protected int bathrooms;
     protected int kitchens; 
@@ -18,32 +18,42 @@ public abstract class Property
     public Property(int rooms, int cubic_Meter, int bathrooms, int kitchens, 
         int garden_Cubic_Meter, int price, String color, String SSN, String street_Number)
     {
-        setRooms_Nr(rooms);
-        this.cubic_Meter = cubic_Meter;
-        this.bathrooms = bathrooms;
-        this.kitchens = kitchens;
-        this.garden_Cubic_Meter = garden_Cubic_Meter;
-        this.price = price;
-        this.color =  color;
-        this.SSN = SSN;
-        this.street_Number = street_Number;
+        this.rooms  = Limit_Verify(rooms, 2, 7, "Room");
+        this.cubic_Meter = Limit_Verify(cubic_Meter, 150, 250, "Interior cubic Meter");
+        this.bathrooms = Limit_Verify(bathrooms, 1, 4, "Bathroom");
+        this.kitchens = Limit_Verify(kitchens, 1, 2, "Kitchen");
+        this.garden_Cubic_Meter = Limit_Verify(garden_Cubic_Meter, 100, 400, "Exterior cubic meter");
+        this.price = Limit_Verify(price, 2000000, 15000000, "Price");
+        this.color =  String_Verify(color);
+        this.SSN = String_Verify(SSN);
+        this.street_Number = String_Verify(street_Number);
         properties_Sold++;
 
     }
 
-    public int getRooms_Nr() //Retrieves the information of rooms set by the user
+
+/*Gets a parameter and checks if the int values are within the limit, 
+if correct then proceed, if false you must input again*/
+    private static int Limit_Verify(int value, int min, int max, String section) 
     {
-        return this.rooms;
+        if(value<min || value>max)
+            {
+                throw new IllegalArgumentException(section + " must be between " + min + " or " + max);
+            }
+        return value;
     }
 
-    public void setRooms_Nr(int rooms) //Checks if the amount of rooms exceeds or undermines the limit
+//Gets a parameter and checks if string values are filled, if correct then proceed, if false you must input again
+    private static String String_Verify(String value)
     {
-        if(rooms<2 || rooms>7)
-        {
-            throw new IllegalArgumentException("Room amount has exceeded the limit (2-7)");
-        }
-        this.rooms = rooms;
+        if(value.isEmpty())
+            {
+                throw new IllegalArgumentException("Error, string is empty");
+            }
+        return value;
     }
+
+
 
 
 
