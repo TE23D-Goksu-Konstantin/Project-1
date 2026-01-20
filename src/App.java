25ec57e3514984a9a960bb39eaccd61687482037
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2026-01-20 12:50:25 +0100

    app structure completed

--- src/App.java
+++ src/App.java
@@ -1,4 +1,4 @@
-import Properties.Property;
+import Properties.Property; //Import of all classes in package "Properties"
 import Properties.Garage;
 import Properties.Townhouse;
 import Properties.Villa;
@@ -11,24 +11,58 @@ public class App
 {
     public static void main(String[] args) throws Exception 
     {
-        Scanner userInputStr = new Scanner(System.in);
+        Scanner userInputStr = new Scanner(System.in); //Keyboard reader -> string for program reader
 
-        Property[] buildings = new Property[100];
-        int buildingCount = 0;
+        Property[] buildings = new Property[100]; //Amount of buildings that can exist at once
+        int buildingCount = 0; //Counts all new registered buildings
 
-        while(buildingCount <= buildings.length)
+        while(buildingCount <= buildings.length) //As long as building count is below max -> continue
             {
-                String menu_Choice = userInputStr.nextLine();
-                int userInputInt = Integer.parseInt(menu_Choice);
                 System.out.println(
                     "1. Register purchase" + 
                     "\n2. Unregister purchase" + 
                     "\n3. List orders" + 
                     "\n4. Count profit"
                 );
-
+                
+                String menu_Choice = userInputStr.nextLine(); //Forces a user input
+                try 
+                {
+                    int userInputInt = Integer.parseInt(menu_Choice); //Translates user input to an integer
+                    switch (userInputInt) {
+                        case 1:
+                            System.out.println("Register purchase selected");
+    
+                            break;
+    
+                        case 2:
+                            System.out.println("Unregister purchase selected");
+    
+                            break;
+    
+                        case 3:
+                            System.out.println("List orders selected");
+    
+                            break;
+    
+                        case 4:
+                            System.out.println("Count profit selected");
+    
+                            break;
+                        
+    
+    
+                        default:
+                            break;
+                    }
+                } 
+                catch (IllegalArgumentException e) 
+                {
+                    System.out.println("Please choose an option to proceed");
+                }
 
             }
 
     }
+
 }
